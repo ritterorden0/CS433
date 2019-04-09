14b85927b9673571966f96329b2c108280c81933
ritterorden0 <43770371+ritterorden0@users.noreply.github.com>
2019-04-08 22:03:16 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUMBER_OF_CUSTOMERS 5
+#define NUMBER_OF_RESOURCES 4
+
+int available[NUMBER_OF_RESOURCES];
+int max[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+int alloc[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+int need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+
+int request_resources(int customer_num, int request[]) {
+
+    return 0;
+}
+
+void release_resources(int customer_num, int release[]) {
+}
+
+int isSafe() {
+    if () {
+        return 0;
+    }
+    return 1;
+}
